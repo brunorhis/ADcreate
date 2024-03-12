dc3b4916883ca65a512bb5814c76b63e01e9c086
Bruno Rhis <brunorhis@gmx.com>
2024-03-12 01:09:03 -0300

    Update createad.ps1

--- createad.ps1
+++ createad.ps1
@@ -1,49 +1,35 @@
-Add-Type -AssemblyName System.Web
+# Importa o módulo ActiveDirectory
+Import-Module ActiveDirectory
 
-# Caminho para o arquivo TXT contendo nomes
-$Global:PathToNamesFile = "C:\Caminho\Para\Seu\Arquivo.txt"
+# Caminho para o arquivo TXT
+$caminhoArquivoTXT = "C:\Caminho\Para\Seu\Arquivo.txt"
 
 # Verifica se o arquivo TXT existe
-if (Test-Path $Global:PathToNamesFile) {
+if (Test-Path $caminhoArquivoTXT) {
     # Lê o conteúdo do arquivo TXT
-    $Global:HumansNames = Get-Content $Global:PathToNamesFile
-} else {
-    Write-Error "O arquivo TXT de nomes não foi encontrado."
-    return
-}
+    $dadosUsuarios = Get-Content $caminhoArquivoTXT
 
-# Definindo o limite de usuários a serem criados
-$limit = 10
+    # Obtém a Unidade Organizacional padrão do domínio
+    $ouPadrao = (Get-ADDomain).DistinguishedName
 
-# Definindo o domínio
-$Global:Domain = "dominio.com"  # Substitua pelo seu domínio
+    # Itera sobre os dados do arquivo TXT para criar os usuários
+    foreach ($linha in $dadosUsuarios) {
+        # Divide a linha em nome e sobrenome
+        $nomeSobrenome = $linha -split '\.'
+        $nomeUsuario = $nomeSobrenome[0]
+        $sobrenomeUsuario = $nomeSobrenome[1]
+        
+        # Gera uma senha aleatória de 5 dígitos
+        $senhaUsuario = Get-Random -Minimum 10000 -Maximum 99999 | ConvertTo-SecureString -AsPlainText -Force
+        
+        # Define o SamAccountName combinando nome e sobrenome
+        $samAccountName = "$nomeUsuario$sobrenomeUsuario"
 
-# Função para obter um item aleatório da lista
-function VulnAD-GetRandom {
-    param (
-        [string[]]$InputList
-    )
-    Get-Random -InputObject $InputList
-}
-
-# Loop para criar usuários
-for ($i=1; $i -le $limit; $i=$i+1) {
-    $firstname = (VulnAD-GetRandom -InputList $Global:HumansNames)
-    $lastname = (VulnAD-GetRandom -InputList $Global:HumansNames)
-    $fullname = "{0} {1}" -f ($firstname , $lastname)
-    $SamAccountName = ("{0}.{1}" -f ($firstname, $lastname)).ToLower()
-    $principalname = "{0}.{1}" -f ($firstname, $lastname)
-    
-    # Gerando uma senha aleatória de 12 caracteres
-    $generated_password = [System.Web.Security.Membership]::GeneratePassword(12, 2)
-    
-    Write-Info "Creating $SamAccountName User"
-    
-    # Tentativa de criar o usuário
-    Try {
-        New-ADUser -Name "$firstname $lastname" -GivenName $firstname -Surname $lastname -SamAccountName $SamAccountName -UserPrincipalName "$principalname@$Global:Domain" -AccountPassword (ConvertTo-SecureString $generated_password -AsPlainText -Force) -PassThru | Enable-ADAccount
-        $Global:CreatedUsers += $SamAccountName
-    } Catch {
-        Write-Error "Error creating user $SamAccountName"
+        # Cria o novo usuário usando a Unidade Organizacional padrão
+        New-ADUser -SamAccountName $samAccountName -UserPrincipalName "$samAccountName@dominio.com" -Name "$nomeUsuario $sobrenomeUsuario" -GivenName $nomeUsuario -Surname $sobrenomeUsuario -Enabled $true -ChangePasswordAtLogon $true -PasswordNeverExpires $false -OrganizationalUnit $ouPadrao -AccountPassword $senhaUsuario
     }
+
+    Write-Host "Usuários criados com sucesso."
+} else {
+    Write-Host "O arquivo TXT não foi encontrado."
 }
